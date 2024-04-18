c261134b353d0c4e45c24db1cf5f5212d06e6a31
Holo <yousseflaraki99@gmail.com>
2024-04-18 10:58:40 +0200

    added array functions for normalizing inputs

--- utils.h
+++ utils.h
@@ -3,30 +3,6 @@
 #define FALSE 0
 #define TRUE 1
 
-int last_start_tick = 0;
-int last_finished_tick = 0;
-int currently_ticking = 0;
-
-void tick_no_delay(int interval) {
-	int current_time = millis();
-	if(current_time - last_finished_tick >= interval) {
-		if(currently_ticking == 0) {
-			last_start_tick = current_time;
-			currently_ticking = 1;
-		}
-
-		int advancement = current_time - last_start_tick;
-
-		if(advancement >= 0 && advancement <= 50) {
-			tone(BUZZER_PIN, 262, 50);
-		} else if(advancement <= 70) {
-			noTone(BUZZER_PIN);
-		} else if(advancement <= 120) {
-			tone(BUZZER_PIN, 262, 50);
-		} else {
-			noTone(BUZZER_PIN);
-			currently_ticking = 0;
-			last_finished_tick = millis();
-		}
-	}
-}
+void tick_no_delay(int interval);
+void populate_array(int size, float* array, float (*func)());
+float average_array(int size, float* array);
--- utils.ino
+++ utils.ino
@@ -1,3 +1,42 @@
 #include "utils.h"
 
-void tick_no_delay(int interval);
+int last_start_tick = 0;
+int last_finished_tick = 0;
+int currently_ticking = 0;
+
+void tick_no_delay(int interval) {
+	int current_time = millis();
+	if(current_time - last_finished_tick >= interval) {
+		if(currently_ticking == 0) {
+			last_start_tick = current_time;
+			currently_ticking = 1;
+		}
+
+		int advancement = current_time - last_start_tick;
+
+		if(advancement >= 0 && advancement <= 50) {
+			tone(BUZZER_PIN, 262, 50);
+		} else if(advancement <= 70) {
+			noTone(BUZZER_PIN);
+		} else if(advancement <= 120) {
+			tone(BUZZER_PIN, 262, 50);
+		} else {
+			noTone(BUZZER_PIN);
+			currently_ticking = 0;
+			last_finished_tick = millis();
+		}
+	}
+}
+
+void populate_array(int size, float* array, float (*func)()) {
+	for(int i = 1; i < size; i++)
+		array[i - 1] = array[i];
+	array[size - 1] = func();
+}
+
+float average_array(int size, float* array) {
+	float sum = 0.0f;
+	for(int i = 0; i < size; i++)
+		sum += array[i];
+	return sum / (float)size;
+}
