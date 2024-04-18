ff575f5952504b303f1bc463ace5f7340bc7e6e2
Holo <yousseflaraki99@gmail.com>
2024-04-18 10:48:56 +0200

    added more input functions

--- board.h
+++ board.h
@@ -1,10 +1,16 @@
+#pragma once
+
 #include <Servo.h>
 
 #define BUTTON_PIN 2
-#define BUZZER_PIN 3
+#define BUZZER_PIN 7
 #define SERVO_PIN 5
 
 void setup_layout();
+
 Servo get_servo();
 
-float get_accelerometer();
+int read_button();
+int read_button_once();
+float read_acceleration_y();
+float read_altitude();
--- board.ino
+++ board.ino
@@ -1,4 +1,5 @@
 #include "board.h"
+#include "utils.h"
 
 Servo servo;
 
@@ -9,4 +10,29 @@ void setup_layout() {
 
 Servo get_servo() {
 	return servo;
+}
+
+int previous_button_state;
+
+int read_button() {
+	if(digitalRead(BUTTON_PIN) == LOW)
+		return TRUE;
+	return FALSE;
+}
+
+int read_button_once() {
+	int previous_cache = previous_button_state;
+	int button_state = read_button();
+	previous_button_state = button_state;
+	if(previous_cache == FALSE && button_state == TRUE)
+		return TRUE;
+	return FALSE;
+}
+
+float read_acceleration_y() {
+	return 0.0f;
+}
+
+float read_altitude() {
+	return 0.0f;
 }
