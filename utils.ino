e22836185b13b2a030faa0292a4ace3cddefa1c4
Holo <yousseflaraki99@gmail.com>
2024-04-19 13:28:25 +0200

    added continuous tick for ascending phase (debugging)

--- utils.ino
+++ utils.ino
@@ -28,6 +28,21 @@ void tick_no_delay(int interval, int frequency) {
 	}
 }
 
+int con_last_change = 0;
+int con_phase = 0;
+
+void tick_continuously_no_delay(int interval, int frequencyA, int frequencyB) {
+	int current_time = millis();
+	if(current_time - con_last_change >= interval) {
+    con_last_change = current_time;
+		con_phase = 1 - con_phase;
+
+		if(con_phase == 0)
+			tone(BUZZER_PIN, frequencyA, interval);
+		else tone(BUZZER_PIN, frequencyB, interval);
+	}
+}
+
 void populate_array(int size, float* array, float (*func)()) {
 	for(int i = 1; i < size; i++)
 		array[i - 1] = array[i];
