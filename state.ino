68c54aaf8dbfbca37d9e5f2afae9b76d33e389d8
Holo <yousseflaraki99@gmail.com>
2024-04-19 13:28:49 +0200

    added temporary debugging prints when switching states

--- state.ino
+++ state.ino
@@ -2,5 +2,18 @@
 
 int current_state;
 
-void change_state(int new_state) { current_state = new_state; }
+void change_state(int new_state) {
+	current_state = new_state;
+	
+	Serial.print("Switching to ");
+	switch(get_current_state()) {
+		case POWER_OFF: Serial.println("powered off."); break;
+		case IDLE: Serial.println("idle."); break;
+		case ASCENDING: Serial.println("ascending."); break;
+		case DESCENDING_PRE_MAIN: Serial.println("descending 1."); break;
+		case DESCENDING_POST_MAIN: Serial.println("descending 2."); break;
+		case GROUNDED: Serial.println("grounded."); break;
+	}
+}
+
 int get_current_state() { return current_state; }
