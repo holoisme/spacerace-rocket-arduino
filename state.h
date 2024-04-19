cfd4d4d4dd5add410015fcac208ce451b0cbc107
Holo <yousseflaraki99@gmail.com>
2024-04-19 12:27:00 +0200

    split the descending state into two (pre/post main launch)

--- state.h
+++ state.h
@@ -3,8 +3,9 @@
 #define POWER_OFF 0
 #define IDLE 1
 #define ASCENDING 2
-#define DESCENDING 3
-#define GROUNDED 4
+#define DESCENDING_PRE_MAIN 3
+#define DESCENDING_POST_MAIN 4
+#define GROUNDED 5
 
 void change_state(int new_state);
 int get_current_state();
