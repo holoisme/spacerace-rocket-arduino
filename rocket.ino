9b3b7fc2d17e790a864ae1cfdd7df14e7a0380c2
Holo <yousseflaraki99@gmail.com>
2024-04-25 00:58:21 +0200

    changed acceleration computations with velocity ones (based on BMP180 sensor)

--- rocket.ino
+++ rocket.ino
@@ -5,7 +5,7 @@
 #include "utils.h"
 #include "config.h"
 
-float acceleration_samples[ACCELERATION_N_SAMPLES];
+// float acceleration_samples[ACCELERATION_N_SAMPLES];
 float altitude_samples[ALTITUDE_N_SAMPLES];
 
 void setup() {
@@ -24,18 +24,26 @@ void loop() {
 		case DESCENDING_POST_MAIN: state_descending_post_main(); break;
 		case GROUNDED: state_grounded(); break;
 	}
-	delay(1);
+	delay(DT);
 }
 
 void update_sensor_samples() {
-	populate_array(ACCELERATION_N_SAMPLES, acceleration_samples, read_acceleration_y);
+	// populate_array(ACCELERATION_N_SAMPLES, acceleration_samples, read_acceleration_y);
 	populate_array(ALTITUDE_N_SAMPLES, altitude_samples, read_altitude);
 }
 
+float get_current_altitude() {
+	return average_array(ALTITUDE_N_SAMPLES, altitude_samples);
+}
+
+float get_current_velocity() {
+	return compute_deltas(ALTITUDE_N_SAMPLES, altitude_samples) / DT_s;
+}
+
 void state_power_off() {
-	noTone(BUZZER_PIN);
 	digitalWrite(LED_BUILTIN, LOW);
-	set_servo(0);
+	set_servo(DEFAULT_SERVO_POSITION);
+	noTone(BUZZER_PIN);
 
 	int button_state = read_button_once();
 	if(button_state == TRUE)
@@ -44,66 +52,60 @@ void state_power_off() {
 
 void state_idle() {
 	digitalWrite(LED_BUILTIN, HIGH);
-	set_servo(90);
+	set_servo(DEFAULT_SERVO_POSITION);
 	tick_no_delay(IDLE_TICK_INTERVAL, IDLE_TICK_FREQUENCY);
 
 	int button_state = read_button_once();
 	if(button_state == TRUE)
-		change_state(POWER_OFF);
+		change_state(ASCENDING);
 
-	float current_acceleration = average_array(ACCELERATION_N_SAMPLES, acceleration_samples);
-	float current_altitude = average_array(ALTITUDE_N_SAMPLES, altitude_samples);
-
-	if(current_acceleration >= IDLE_ACCELERATION_THRESHOLD && current_altitude >= IDLE_ALTITUDE_THRESHOLD)
+	if(get_current_velocity() >= IDLE_VELOCITY_THRESHOLD && get_current_altitude() >= IDLE_ALTITUDE_THRESHOLD)
 		change_state(ASCENDING);
 }
 
 void state_ascending() {
 	digitalWrite(LED_BUILTIN, HIGH);
 	set_servo(DEFAULT_SERVO_POSITION);
-	tick_continuously_no_delay(4000, 250, 350);
+	tick_continuously_no_delay(1000, 250, 350);
 
 	int button_state = read_button_once();
 	if(button_state == TRUE)
 		change_state(DESCENDING_PRE_MAIN);
 
-	float current_acceleration = average_array(ACCELERATION_N_SAMPLES, acceleration_samples);
-	if(current_acceleration <= ASCTODES_ACCELERATION_THRESHOLD)
+	if(get_current_velocity() <= ASCTODES_VELOCITY_THRESHOLD)
 		change_state(DESCENDING_PRE_MAIN);
 }
 
 void state_descending_pre_main() {
-	noTone(BUZZER_PIN);
 	digitalWrite(LED_BUILTIN, HIGH);
 	set_servo(DEFAULT_SERVO_POSITION);
+	tick_continuously_no_delay(1000, 150, 250);
 
 	int button_state = read_button_once();
 	if(button_state == TRUE)
 		change_state(DESCENDING_POST_MAIN);
 	
-	float current_altitude = average_array(ALTITUDE_N_SAMPLES, altitude_samples);
-	if(current_altitude <= DEPLOY_ALTITUDE_THRESHOLD)
+	if(get_current_altitude() <= DEPLOY_ALTITUDE_THRESHOLD)
 		change_state(DESCENDING_POST_MAIN);
 }
 
 void state_descending_post_main() {
-	noTone(BUZZER_PIN);
 	digitalWrite(LED_BUILTIN, HIGH);
 	set_servo(DEPLOY_SERVO_POSITION);
+	tick_continuously_no_delay(1000, 100, 250);
 
 	int button_state = read_button_once();
 	if(button_state == TRUE)
 		change_state(GROUNDED);
 
-	float current_acceleration = average_array(ACCELERATION_N_SAMPLES, acceleration_samples);
-	if(abs(current_acceleration) <= GROUNDED_ACCELERATION)
-		change_state(DESCENDING_PRE_MAIN);
+	if(abs(get_current_velocity()) <= GROUNDED_VELOCITY)
+		change_state(GROUNDED);
 }
 
 void state_grounded() {
-	noTone(BUZZER_PIN);
 	digitalWrite(LED_BUILTIN, HIGH);
 	set_servo(DEPLOY_SERVO_POSITION);
+	noTone(BUZZER_PIN);
 
 	int button_state = read_button_once();
 	if(button_state == TRUE)
