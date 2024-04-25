4060302e4af75e1abd453bb3304543389aa89f60
Holo <yousseflaraki99@gmail.com>
2024-04-25 13:54:40 +0200

    fixed null samples taken into account

--- utils.ino
+++ utils.ino
@@ -43,22 +43,28 @@ void tick_continuously_no_delay(int interval, int frequencyA, int frequencyB) {
 	}
 }
 
+int N_ALTITUDE_SAMPLES = 0;
+
 void populate_array(int size, float* array, float (*func)()) {
 	for(int i = 1; i < size; i++)
 		array[i - 1] = array[i];
 	array[size - 1] = func();
+
+	N_ALTITUDE_SAMPLES++;
+	if(N_ALTITUDE_SAMPLES >= size)
+		N_ALTITUDE_SAMPLES = size;
 }
 
 float average_array(int size, float* array) {
 	float sum = 0.0f;
-	for(int i = 0; i < size; i++)
+	for(int i = size - N_ALTITUDE_SAMPLES; i < size; i++)
 		sum += array[i];
-	return sum / (float)size;
+	return sum / (float)N_ALTITUDE_SAMPLES;
 }
 
 float compute_deltas(int size, float *array) {
 	float sum = 0.0f;
-	for(int i = 1; i < size; i++)
+	for(int i = size - N_ALTITUDE_SAMPLES + 1; i < size; i++)
 		sum += array[i] - array[i - 1];
-	return sum / (float)(size - 1);
+	return sum / (float)(N_ALTITUDE_SAMPLES - 1);
 }
